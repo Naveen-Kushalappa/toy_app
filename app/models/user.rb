dacdca68cfd4e8fe485eca0fd6e68178244c31aa
naveen.kushalappa@porteamedical.com <naveen.kushalappa@porteamedical.com>
2018-05-04 16:29:33 +0530

    Scfollded user and microposts with validation

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates :name , length: { maximum: 15 },
+            presence: true
+
+  validates :email , length: { maximum: 15 },
+            presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'application#hi'
+  resources :microposts
+  resources :users
+  root 'users#index'
+#  root 'application#hi'
 
 end
